9b2da7f222c5df2dc29c549270d26fa2f932790f
Payoppol <payosf05@gmail.com>
2026-09-02 09:36:13 -0600

    cambios de codigo y pocicion de archivos

--- EngineXd/Grafica3DEngine/include/Engine.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Api.h"
-extern "C"
-{
-	ENGINE_API const wchar_t* Engine_GetName() noexcept;
-	ENGINE_API int Engine_RunSmokeTest() noexcept;
-}
--- EngineXd/Grafica3DEngine/include/Engine/Engine.h
+++ EngineXd/Grafica3DEngine/include/Engine/Engine.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include "API.h"
+
+extern "C" {
+	ENGINE_API const wchar_t* Engine_GetName() noexcept;
+	ENGINE_API int Engine_RunSmokeTest() noexcept;
+}
